370921cb652c8dc992dfa2c537b173bd3688c3a7
Isaac Post <ipost@covermymeds.com>
2017-11-05 22:42:56 -0500

    move bots into bots dir; add randomized dimensions

--- run_game.sh
+++ run_game.sh
@@ -12,10 +12,16 @@ if ls *-*.log 1> /dev/null 2>&1; then
   rm -f *-*.log
 fi
 
+random_dimension()
+{
+  awk -v min=120 -v max=400 'BEGIN{srand(); print int(min+rand()*(max-min+1))}'
+}
+
 #./halite_osx -d "180 180" "RUST_BACKTRACE=1 target/release/MyBot" "./ipostv1"
 #./halite_osx -d "180 180" "RUST_BACKTRACE=1 target/release/MyBot" "./VanillaSettler"
 
-./halite_osx -d "150 150" "RUST_BACKTRACE=1 target/release/MyBot" "./cheesebot"
+./halite_osx -d "150 150" "RUST_BACKTRACE=1 target/release/MyBot" "./bots/cheesebot"
+#./halite_osx -d "$(random_dimension) $(random_dimension)" "RUST_BACKTRACE=1 target/release/MyBot" "./bots/cheesebot"
 
 # ship 75 around turn 72 stops despite thrust command
 #./halite_osx -d "336 224" -s 3644293869 "RUST_BACKTRACE=1 target/release/MyBot" "./ipostv1"
